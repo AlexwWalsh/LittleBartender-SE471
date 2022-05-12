397ca6f1c0f54bb870a0cdc8f6740e1c66c0a806
CFPActual <94029946+CFPActual@users.noreply.github.com>
2022-05-12 03:55:24 -0700

    Add files via upload
    Added :
    PopupCreator.java - it's only purpose is to create new objects of the RecipePopup class.
    
    Modified:
    MainMenuScreen.createScrollPane() -  to get the popup making process  started, a popupManager object is created here. ObjectPool and popup creation is elsewhere now
    PopupManager -  no longer takes in an ObjectPool, it gets the poolInstance.; it also communes with the Object Pool to begin "getting" popups to display
    ObjectPool .removeObject() renamed  to reuseObject() and modified to work in concert with waitForObject() which is also modified to work better with the PopupManager.
    RecipePopup.returnIt()  - added to facilitate object release to the pool
    RecipePopup.popupImage() - dissected to breakout portions of the code that are important to object reuse.  
    RecipePopup..getURL() - provides the image retrieval information
    RecipePopup..setPic() -  sets the pictures for each drink popup; and because it's returned variable is global now, it can be used for reuse
    RecipePopup.refreshPane() -  used strictly for reused popup objects - refreshes the information in the target popup with the data of the passed in recipe
    RecipePopup class variables textAreaI, textAread, rName, popupPic are declared globally now to allow visibility to refreshPane() .
    
    ToDo (maybe):
    Migrate the favoriting functionality to .refreshPane()

--- src/PopupManager.java
+++ src/PopupManager.java
@@ -1,22 +1,32 @@
+import javax.swing.JOptionPane;
+
 /**
- *  PopupManager.java gets popups from the RecipePopup object pool
- *  It also provides a connection between the Object pool
- *  and the RecipePopup class by passing a reference to the
- *  ObjectPool so ObjectPool class methods can be used there.
+ *  Creates the ObjectPool; Gets popups both new and reusable 
  */
 public class PopupManager implements PopupManagerIF {
-    private ObjectPool pool;
-    private RecipePopup rp;
+    private static ObjectPool pool;
+    private Recipe rp;
+    int poolSize = 2; //only this many new will be created; after this pool only upon release
 
     /*constructor*/
-    public PopupManager(ObjectPool p){
-       pool = p;
+    public PopupManager(Recipe r){
+    	rp = r;
+    	pool = ObjectPool.getPoolInstance(poolSize);
+    	getPopup(rp);
     }//end of constructor
     
-    @Override
-    public Object getPopUp(Recipe r){  //return object or popup??
-        rp = new RecipePopup(pool, r);//pass on the recipe and
-      return rp;
-    }//end of getPopUp
-    
+    /*This is where the popup creation begins*/
+    public void getPopup(Recipe r) {
+        /* If the number of recipe popup instances is less than the limit we can create more; if not a warning is displayed
+         * SOME REFINEMENTS ARE NEED HERE TO MAKE IT MORE ROBUST AND 100% PREDICTABLE
+         */
+    	RecipePopup pop = (RecipePopup) pool.getObject(r); //the returned object must be typed casted to RecipePopup
+    	    if(pop == null ){ 
+				JOptionPane.showMessageDialog(null, "You are only allowed " + poolSize + " recipe popups.\n Close a popup to see more recipes."); 
+				pop = (RecipePopup) pool.waitForObject(r); //can't create more objects so wait for a pool object to be available
+				if(pop != null ) {pop.showPopup();} //only get valid popups from waitForObject()
+    	    }	
+    	    else {pop.showPopup();}
+    }//end of getPopup
+   
 }//end of popupRequester class
